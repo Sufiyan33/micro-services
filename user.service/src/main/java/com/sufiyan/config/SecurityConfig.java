fe44fdea466f9c91e65ca060091d6553a6a239e5
Sufiyan33 <Basu700736@gmail.com>
2023-07-02 12:49:25 +0530

    Commented security related dependencies as of now

--- user.service/src/main/java/com/sufiyan/config/SecurityConfig.java
+++ user.service/src/main/java/com/sufiyan/config/SecurityConfig.java
@@ -1,18 +1,13 @@
 package com.sufiyan.config;
 
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@EnableWebSecurity
-public class SecurityConfig {
-
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().requestMatchers(HttpMethod.POST).hasRole("Admin").anyRequest()
-				.authenticated();
-
-		return http.build();
-	}
-
-}
+/*
+ * @EnableWebSecurity public class SecurityConfig {
+ * 
+ * public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+ * http.csrf().disable().authorizeRequests().requestMatchers(HttpMethod.POST).
+ * hasRole("Admin").anyRequest() .authenticated();
+ * 
+ * return http.build(); }
+ * 
+ * }
+ */
