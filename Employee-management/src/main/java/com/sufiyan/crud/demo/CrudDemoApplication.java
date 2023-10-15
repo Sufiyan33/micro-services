c6989179d37bf0a541e7932699632be4cb6989e5
Sufiyan33 <Basu700736@gmail.com>
2023-10-16 00:16:46 +0530

    Enables eureka client

--- Employee-management/src/main/java/com/sufiyan/crud/demo/CrudDemoApplication.java
+++ Employee-management/src/main/java/com/sufiyan/crud/demo/CrudDemoApplication.java
@@ -4,9 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class CrudDemoApplication {
 
 	private static final Logger log = LoggerFactory.getLogger(CrudDemoApplication.class.getName());
