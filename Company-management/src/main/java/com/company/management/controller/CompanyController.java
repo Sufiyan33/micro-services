743eb829c51fc887137775ce4c72004574117881
Sufiyan33 <Basu700736@gmail.com>
2023-10-15 21:54:07 +0530

    Adding new micro-services company-management

---
+++ Company-management/src/main/java/com/company/management/controller/CompanyController.java
@@ -0,0 +1,67 @@
+package com.company.management.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.company.management.entity.Company;
+import com.company.management.service.CompanyService;
+
+@RestController
+@RequestMapping("/company")
+public class CompanyController {
+
+	private static final Logger log = LoggerFactory.getLogger(CompanyController.class.getSimpleName());
+	
+	@Autowired
+	CompanyService service;
+	
+	@PostMapping("/save")
+	public Company saveCompany(@RequestBody Company company) {
+		log.info("Saving Company data");
+		return service.save(company);
+	}
+
+	@PostMapping("/saveAll")
+	public List<Company> saveAllCompany(@RequestBody List<Company> company) {
+		return service.saveAllDetails(company);
+	}
+
+	@GetMapping("/com/{id}")
+	public Company getCompany(@PathVariable("id") long companyId) {
+		return service.fetchCompany(companyId);
+	}
+
+	@GetMapping("/allCompany")
+	public List<Company> getAllCompaniesDetails() {
+		return service.fetchAllCompanyDetails();
+	}
+
+	@PutMapping("/update/{id}")
+	public Company updateCompany(@PathVariable("id") long companyId, @RequestBody Company company) {
+		log.info("Company name: " + company.getCompanyName() + " company Id: " + company.getCompanyId() + " companyId: " + companyId
+				+ " & company object is : " + company);
+		return service.updateCompanyDetails(company, companyId);
+	}
+
+	@PatchMapping("updates/{id}")
+	public Company updatePartially(@PathVariable("id") long companyId, @RequestBody Company company) {
+		return service.updateCompany(companyId, company);
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public String deleteCompanyById(@PathVariable long companyId) {
+		return service.deleteRecord(companyId);
+	}
+}
