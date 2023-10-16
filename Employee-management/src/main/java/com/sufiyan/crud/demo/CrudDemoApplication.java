17106eed5d0a17c4e925646f50215761e3566aa9
Sufiyan33 <Basu700736@gmail.com>
2023-10-16 09:18:12 +0530

    Added restTemplate Bean in main class

--- Employee-management/src/main/java/com/sufiyan/crud/demo/CrudDemoApplication.java
+++ Employee-management/src/main/java/com/sufiyan/crud/demo/CrudDemoApplication.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -15,6 +17,11 @@ public class CrudDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrudDemoApplication.class, args);
 		log.info("This is the main class of spring boot");
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
